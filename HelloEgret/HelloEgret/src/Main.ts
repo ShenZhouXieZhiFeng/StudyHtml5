bd8b0057be57853cead0dbfc89d958fae03743d6
Stone <827443469@qq.com>
2018-03-12 14:23:04 +0800

    创建白鹭引擎游戏项目

---
+++ HelloEgret/HelloEgret/src/Main.ts
@@ -0,0 +1,121 @@
+class Main extends egret.DisplayObjectContainer {
+
+    public constructor() {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+        this.testFunc();
+    }
+
+    //测试ts代码的入口
+    public testFunc() : void
+    {
+        
+    }
+
+    private onAddToStage(event: egret.Event) {
+        //初始化Resource资源加载库
+        //initiate Resource loading library
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.loadConfig("resource/default.res.json", "resource/");
+    }
+
+    /**
+     * 配置文件加载完成,开始预加载preload资源组。
+     * configuration file loading is completed, start to pre-load the preload resource group
+     */
+    private onConfigComplete(event: RES.ResourceEvent): void {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+        RES.addEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+        RES.loadGroup("preload");
+    }
+
+    /**
+     * preload资源组加载完成
+     * Preload resource group is loaded
+     */
+    private onResourceLoadComplete(event: RES.ResourceEvent) {
+        if (event.groupName == "preload") {
+            RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+            RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+            this.createGameScene();
+        }
+    }
+
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    private onItemLoadError(event: RES.ResourceEvent) {
+        console.warn("Url:" + event.resItem.url + " has failed to load");
+    }
+
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    private onResourceLoadError(event: RES.ResourceEvent) {
+        //TODO
+        console.warn("Group:" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        //Ignore the loading failed projects
+        this.onResourceLoadComplete(event);
+    }
+
+    /**
+     * preload资源组加载进度
+     * Loading process of preload resource group
+     */
+    private onResourceProgress(event: RES.ResourceEvent) {
+        if (event.groupName == "preload") {
+        }
+    }
+
+    private icons: egret.Bitmap[] = [];
+
+    /**
+     * 创建游戏场景
+     * Create a game scene
+     */
+    private createGameScene() {
+        // this.stage.removeChild(this);
+        let width = this.stage.stageWidth - 32;
+        let height = this.stage.stageHeight - 32;
+        for (let i = 0; i < 10000; i++) {
+            let icon = this.createBitmapByName("star_png");
+            icon.x = Math.random() * width;
+            icon.y = Math.random() * height;
+            this.addChild(icon);
+            this.icons.push(icon);
+        }
+
+        this.stage.addEventListener(egret.Event.ENTER_FRAME, function () {
+            let width = this.stage.stageWidth - 32;
+            let length = this.icons.length;
+            for (let i = 0; i < length; i++) {
+                let icon = this.icons[i];
+                icon.x += 4;
+                if(icon.x > width) {
+                    icon.x = 0;
+                }
+            }
+        }, this);
+    }
+
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    private createBitmapByName(name: string) {
+        let result = new egret.Bitmap();
+        let texture: egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+}
+
+
---
+++ HelloEgret/HelloEgret/src/first/stone.ts
@@ -0,0 +1,13 @@
+class stone {
+
+	name:string;
+
+	public constructor(name:string) {
+		this.name = name;
+	}
+
+	public print() : void
+	{
+		console.log("your name is :" + name);
+	}
+}
